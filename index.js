528d424a39065954017fd4811d93ee65b1439b77
alreadytakenlala <1285380545@qq.com>
2019-08-30 09:33:24 +0800

    完善

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import condition from "./lib/mobility/Condition";
 import deposition from "./lib/mobility/Deposition";
 import increase from "./lib/mobility/Increase";
 import cascading from "./lib/mobility/Cascading";
+import catalog from "./lib/mobility/Catalog";
 
 export let Mobility = mobility;
 export let Condition = condition;
 export let Deposition = deposition;
 export let Increase = increase;
 export let Cascading = cascading;
+export let Catalog = catalog;
